e3b815d9210f14d2976b635eaadbcba116154791
DebarajLenka85 <debarajcool85@gmail.com>
2025-08-31 16:07:44 +0000

    commit latest

---
+++ Netflix_Prj/models/staging/src_movies.sql
@@ -0,0 +1,10 @@
+{{ config(materialized = 'table') }}
+
+WITH raw_movies AS (
+    SELECT * FROM {{ source('netflix', 'r_movies') }}
+)
+SELECT 
+    movieId AS movie_id,
+    title,
+    genres
+FROM raw_movies
